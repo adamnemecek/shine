d10b35acc8758928c63e3b3597792c12d956832e
gzp <z.p.gaal@gmail.com>
2017-06-22 01:20:01 +0200

    more idiomatic spsc state + threaded test

--- src/container/spscstate.rs
+++ src/container/spscstate.rs
@@ -1,41 +1,35 @@
-#![allow(dead_code)]
-
 use std::cell::{UnsafeCell};
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc};
 use std::ops::{Deref, DerefMut};
 
 
+type AtomicFlag = AtomicUsize;
+
+/// Triple buffer that uses atomic operations to rotate the 3 buffers during consume/produce operations
+/// It is not an spsc queue as some massages can be dropped based.
 // flag bits:
 // newWrite   = (flags & 0x40)
 // produceIndex = (flags & 0x30) >> 4       buffer to be produced, write to
 // intermediateIndex = (flags & 0xC) >> 2   intermediate buffer (transit zone)
 // consumeIndex  = (flags & 0x3)            buffer to consume, consume from
-
-type Flag = usize;
-type AtomicFlag = AtomicUsize;
-
-pub struct SPSCState<T> {
+struct TripleBuffer<T> {
     buffers: UnsafeCell<[T; 3]>,
     flags: AtomicFlag,
 }
 
-pub struct Producer<'a, T: 'a>(&'a SPSCState<T>, usize);
+unsafe impl<T> Sync for TripleBuffer<T> {}
 
-pub struct Consumer<'a, T: 'a>(&'a SPSCState<T>, usize);
-
-
-unsafe impl<T> Sync for SPSCState<T> {}
-
-impl<T: Default> SPSCState<T> {
-    pub fn new() -> SPSCState<T> {
-        SPSCState {
+impl<T: Default> TripleBuffer<T> {
+    pub fn new() -> TripleBuffer<T> {
+        TripleBuffer {
             buffers: UnsafeCell::new([Default::default(), Default::default(), Default::default()]),
             flags: AtomicFlag::new(0x6),
         }
     }
 }
 
-impl<T> SPSCState<T> {
+impl<T> TripleBuffer<T> {
     /// Gets the index of the buffer to produce
     fn get_produce_index(&self) -> usize {
         let produce_index = (self.flags.load(Ordering::SeqCst) & 0x30) >> 4;
@@ -46,13 +40,13 @@ impl<T> SPSCState<T> {
     /// If a the new flag was set, the index to the (new) consume buffer is returned, otherwise Err
     /// is returned.
     /// Index of the produce buffer is not modified.
-    fn try_get_consume_index(&self) -> usize {
+    fn try_get_consume_index(&self) -> Result<usize, ()> {
         let mut old_flags = self.flags.load(Ordering::Acquire);
         let mut new_flags: usize;
         loop {
             if (old_flags & 0x40) == 0 {
                 // nothing new, no need to swap
-                return usize::max_value();
+                return Err(());
             }
             // clear the "new" bit and swap the indices of consume and intermediate buffers
             new_flags = (old_flags & 0x30) | ((old_flags & 0x3) << 2) | ((old_flags & 0xC) >> 2);
@@ -62,7 +56,7 @@ impl<T> SPSCState<T> {
                 Err(x) => old_flags = x,
             }
         }
-        new_flags & 0x3
+        Ok(new_flags & 0x3)
     }
 
     /// Swaps intermediate and (new)produced buffers and sets the new flag.
@@ -79,56 +73,104 @@ impl<T> SPSCState<T> {
             }
         }
     }
-
-    pub fn produce(&self) -> Producer<T> { Producer::new(self) }
-
-    pub fn consume(&self) -> Consumer<T> { Consumer::new(self) }
 }
 
 
-impl<'a, T> Producer<'a, T> {
-    fn new(owner: &'a SPSCState<T>) -> Producer<'a, T> {
-        Producer(owner, owner.get_produce_index())
+/// Producer part of the communication.
+pub struct Producer<T>(Arc<TripleBuffer<T>>);
+
+// The receiver can be sent from place to place, so long as it
+// is not used to receive non-sendable things.
+unsafe impl<T: Send> Send for Producer<T> {}
+
+//impl<T> !Sync for Producer<T> { }
+
+impl<T> Producer<T> {
+    pub fn send_buffer(&self) -> Result<RefProduced<T>, ()> {
+        Ok(RefProduced(&self.0, self.0.get_produce_index()))
     }
 }
 
-impl<'a, T> Drop for Producer<'a, T> {
+impl<T: Copy> Producer<T> {
+    pub fn send(&self, value: T) -> Result<(), ()> {
+        match self.send_buffer() {
+            Ok(mut b) => {
+                *b = value;
+                Ok(())
+            }
+            Err(_) => Err(())
+        }
+    }
+}
+
+/// Reference to the buffer held by the producer
+pub struct RefProduced<'a, T: 'a>(&'a TripleBuffer<T>, usize);
+
+impl<'a, T> Drop for RefProduced<'a, T> {
     fn drop(&mut self) {
         self.0.set_produce();
     }
 }
 
-impl<'a, T> Deref for Producer<'a, T> {
+impl<'a, T> Deref for RefProduced<'a, T> {
     type Target = T;
-
     fn deref(&self) -> &T {
         unsafe { &(*self.0.buffers.get())[self.1] }
     }
 }
 
-impl<'a, T> DerefMut for Producer<'a, T> {
+impl<'a, T> DerefMut for RefProduced<'a, T> {
     fn deref_mut(&mut self) -> &mut T {
         unsafe { &mut (*self.0.buffers.get())[self.1] }
     }
 }
 
 
-impl<'a, T> Consumer<'a, T> {
-    fn new(owner: &'a SPSCState<T>) -> Consumer<'a, T> {
-        Consumer(owner, owner.try_get_consume_index())
+/// Consumer part of the communication
+pub struct Consumer<T>(Arc<TripleBuffer<T>>);
+
+// The consumer can be sent from place to place, so long as it
+// is not used to receive non-sendable things.
+unsafe impl<T: Send> Send for Consumer<T> {}
+
+//impl<T> !Sync for Consumer<T> { }
+
+impl<T> Consumer<T> {
+    pub fn receive_buffer(&self) -> Result<RefConsumed<T>, ()> {
+        match self.0.try_get_consume_index() {
+            Ok(idx) => Ok(RefConsumed(&self.0, idx)),
+            Err(_) => Err(())
+        }
     }
 }
 
-impl<'a, T> Deref for Consumer<'a, T> {
-    type Target = T;
+impl<T: Copy> Consumer<T> {
+    pub fn receive(&self) -> Result<T, ()> {
+        match self.receive_buffer() {
+            Ok(b) => Ok(*b),
+            Err(_) => Err(())
+        }
+    }
+}
 
+/// Reference to the buffer held by the consumer
+pub struct RefConsumed<'a, T: 'a>(&'a TripleBuffer<T>, usize);
+
+impl<'a, T> Deref for RefConsumed<'a, T> {
+    type Target = T;
     fn deref(&self) -> &T {
         unsafe { &(*self.0.buffers.get())[self.1] }
     }
 }
 
-impl<'a, T> DerefMut for Consumer<'a, T> {
+impl<'a, T> DerefMut for RefConsumed<'a, T> {
     fn deref_mut(&mut self) -> &mut T {
         unsafe { &mut (*self.0.buffers.get())[self.1] }
     }
+}
+
+
+pub fn channel<T: Default>() -> (Producer<T>, Consumer<T>) {
+    let a = Arc::new(TripleBuffer::new());
+    (Producer(a.clone()), Consumer(a.clone()))
 }
