b9355e3c88bc345087a540d6597d86ea58bbd74c
gzp <z.p.gaal@gmail.com>
2017-11-03 08:06:54 +0100

    store

--- engine/src/container/store.rs
+++ engine/src/container/store.rs
@@ -2,11 +2,10 @@
 
 
 use std::fmt;
-use std::rc::Rc;
-use std::cell;
-use std::cell::RefCell;
+use std::sync::*;
 use std::hash::Hash;
 use std::collections::HashMap;
+use std::marker::PhantomData;
 
 
 /// Trait for resource id
@@ -20,7 +19,7 @@ pub trait Data {
 
 
 /// Trait to create value items from the resource id
-pub trait Factory<Key: Id, Value: Data> {
+pub trait Factory<Key: Id, Value: Data>: Send {
     /// Request the creation of an item. The item is not required immediately, but
     /// a create call will be triggered soon.
     fn request(&mut self, id: &Key) -> Value;
@@ -31,53 +30,153 @@ pub trait Factory<Key: Id, Value: Data> {
 
 
 /// An entry in the store.
-#[derive(PartialEq, Eq, Debug)]
-pub enum Entry<Key: Id, Value: Data> {
-    Pending(Key, Value),
-    Ready(Value),
+#[derive(Debug)]
+struct Entry<Value: Data> {
+    is_ready: bool,
+    value: Value,
+}
+
+
+/// Requests for the missing resources.
+struct Requests<Key: Id, Value: Data> {
+    factory: Box<Factory<Key, Value>>,
+    requests: HashMap<Key, Arc<Entry<Value>>>,
+}
+
+impl<Key: Id, Value: Data> Requests<Key, Value> {
+    fn process_requests(&mut self, resources: &mut HashMap<Key, Arc<Entry<Value>>>) {
+        let factory = &mut self.factory;
+        self.requests.retain(|id, value| {
+            if let Some(new_value) = factory.create(id) {
+                {
+                    unsafe {
+                        let us = &**value as *const _ as *mut Entry<Value>;
+                        (*us).value = new_value;
+                        (*us).is_ready = true;
+                    }
+                }
+                resources.insert(id.clone(), value.clone());
+                false
+            } else {
+                true
+            }
+        });
+    }
+}
+
+
+/// Resource store. Resources are constants and can be created/requested through the store only.
+pub struct Store<Key: Id, Value: Data> {
+    resources: RwLock<HashMap<Key, Arc<Entry<Value>>>>,
+    requests: Mutex<Requests<Key, Value>>,
+}
+
+impl<Key: Id, Value: Data> Store<Key, Value> {
+    /// Creates a new store with the given factory.
+    pub fn new<F: 'static + Sized + Factory<Key, Value>>(factory: F) -> Store<Key, Value> {
+        Store {
+            resources: RwLock::new(HashMap::new()),
+            requests: Mutex::new(Requests {
+                factory: Box::new(factory),
+                requests: HashMap::new()
+            }),
+        }
+    }
+
+    /// Process the requests.
+    pub fn update(&self) {
+        let mut resources = self.resources.try_write().unwrap();
+        let mut requests = self.requests.lock().unwrap();
+        requests.process_requests(&mut resources);
+    }
+
+    /// Removes unreferenced resources.
+    pub fn drain_unused(&self) {
+        let mut resources = self.resources.try_write().unwrap();
+        let mut requests = self.requests.lock().unwrap();
+
+        requests.requests.clear();
+        resources.retain(|_, v| {
+            Arc::strong_count(v) > 1
+        });
+    }
+
+    pub fn read<'a>(&'a self) -> StoreUse<Key, Value> {
+        StoreUse {
+            resources: self.resources.read().unwrap(),
+            requests: &self.requests,
+        }
+    }
+}
+
+
+pub struct StoreUse<'a, Key: 'a + Id, Value: 'a + Data> {
+    resources: RwLockReadGuard<'a, HashMap<Key, Arc<Entry<Value>>>>,
+    requests: &'a Mutex<Requests<Key, Value>>,
+}
+
+
+impl<'a, Key: Id, Value: Data> StoreUse<'a, Key, Value> {
+    fn request_unchecked(&self, id: &Key) -> Ref<Key, Value> {
+        let mut requests = self.requests.lock().unwrap();
+        // find resource in the requests
+        if let Some(request) = requests.requests.get(id) {
+            return Ref::new(Some(request.clone()));
+        }
+
+        let value = requests.factory.request(id);
+        let request = Arc::new(Entry {
+            is_ready: false,
+            value: value,
+        });
+        requests.requests.insert(id.clone(), request.clone());
+        Ref::new(Some(request))
+    }
+
+    /// Gets a loaded resource. If resource is not found, a none reference is returned
+    pub fn get(&self, id: &Key) -> Ref<Key, Value> {
+        Ref::new(self.resources.get(id).map(|r| r.clone()))
+    }
+
+    /// Requests a resource to be loaded without taking a reference to it.
+    pub fn request(&self, id: &Key) {
+        if !self.resources.contains_key(id) {
+            self.request_unchecked(id);
+        }
+    }
+
+    /// Gets a resource by the given id. If the resource is not loaded, reference to pending resource is
+    /// returned and the missing is is enqued in the request list.
+    pub fn get_or_request(&self, id: &Key) -> Ref<Key, Value> {
+        let resource = self.get(id);
+        if resource.is_some() {
+            resource
+        } else {
+            self.request_unchecked(id)
+        }
+    }
 }
 
 
 /// Reference to an entry in the store
-#[derive(PartialEq, Eq, Debug)]
+#[derive(/*PartialEq, Eq,*/ Debug)]
 pub struct Ref<Key: Id, Value: Data> {
-    value: Option<Rc<RefCell<Entry<Key, Value>>>>,
+    value: Option<Arc<Entry<Value>>>,
+    phantom: PhantomData<Key>,
 }
 
 impl<Key: Id, Value: Data> Ref<Key, Value> {
-    fn new(value: Rc<RefCell<Entry<Key, Value>>>) -> Ref<Key, Value> {
+    fn new(value: Option<Arc<Entry<Value>>>) -> Ref<Key, Value> {
         Ref {
-            value: Some(value),
+            value: value,
+            phantom: PhantomData,
         }
     }
 
     pub fn none() -> Ref<Key, Value> {
         Ref {
             value: None,
-        }
-    }
-
-    /// Checks if the referenced resource is loaded.
-    pub fn is_ready(&self) -> bool {
-        if let Some(ref value) = self.value {
-            match *value.borrow() {
-                Entry::Ready(..) => true,
-                _ => false
-            }
-        } else {
-            true
-        }
-    }
-
-    /// Checks if the referenced resource is pending.
-    pub fn is_pending(&self) -> bool {
-        if let Some(ref value) = self.value {
-            match *value.borrow() {
-                Entry::Pending(..) => true,
-                _ => false
-            }
-        } else {
-            true
+            phantom: PhantomData,
         }
     }
 
@@ -91,162 +190,40 @@ impl<Key: Id, Value: Data> Ref<Key, Value> {
         self.value.is_none()
     }
 
-    /// Returns non-mutable reference to the data.
-    pub fn borrow(&self) -> cell::Ref<Entry<Key, Value>> {
+    /// Checks if the reference is ready
+    pub fn is_ready(&self) -> bool {
         if let Some(ref value) = self.value {
-            value.borrow()
+            value.is_ready
+        } else {
+            false
+        }
+    }
+
+    /// Checks if the reference is pending
+    pub fn is_pending(&self) -> bool {
+        if let Some(ref value) = self.value {
+            !value.is_ready
+        } else {
+            false
+        }
+    }
+
+    pub fn release(&mut self) {
+        self.value = None;
+    }
+
+    // Returns non-mutable reference to the data.
+    /* pub fn borrow(&self) -> Ref<Value> {
+        if let Some(ref value) = self.value {
+            &value.value
         } else {
             panic!("Dereferencing an ampty reference")
         }
-    }
+    } */
 }
 
-struct Shared<Key: Id, Value: Data> {
-    factory: Box<Factory<Key, Value>>,
-    requests: Vec<Rc<RefCell<Entry<Key, Value>>>>,
-}
-
-impl<Key: Id, Value: Data> Shared<Key, Value> {
-    fn process_requests(&mut self) {
-        let factory = &mut self.factory;
-        self.requests.retain(|request| {
-            let mut request = request.borrow_mut();
-            let value = match *request {
-                Entry::Pending(ref id, _) => { factory.create(id) }
-                _ => { panic!("Non-panding requests") }
-            };
-
-            if let Some(value) = value {
-                *request = Entry::Ready(value);
-                false
-            } else {
-                true
-            }
-        });
-    }
-}
-
-
-/// Resource store. Resources are constants and can be created/requested through the store only.
-pub struct Store<Key: Id, Value: Data> {
-    resources: HashMap<Key, Rc<RefCell<Entry<Key, Value>>>>,
-    shared: RefCell<Shared<Key, Value>>,
-}
-
-impl<Key: Id, Value: Data> Store<Key, Value> {
-    /// Creates a new store with the given factory.
-    pub fn new<F: 'static + Sized + Factory<Key, Value>>(factory: F) -> Store<Key, Value> {
-        Store {
-            resources: HashMap::new(),
-            shared: RefCell::new(Shared {
-                factory: Box::new(factory),
-                requests: vec!()
-            }),
-        }
-    }
-
-    /// Returns the number of items in the store.
-    pub fn len(&self) -> usize {
-        self.resources.len()
-    }
-
-    /// Returns if there are any items in the store.
-    pub fn is_empty(&self) -> bool {
-        self.resources.is_empty()
-    }
-
-    /// Returns the number of requested (but not created) items.
-    pub fn request_len(&self) -> usize {
-        self.shared.borrow().requests.len()
-    }
-
-    /// Returns if there are any requested (but not created) items.
-    pub fn has_request(&self) -> bool {
-        !self.shared.borrow().requests.is_empty()
-    }
-
-    /// Gets resource by the given id. If resource is not loaded an empty reference is returned
-    /// and no request is made to the loader.
-    pub fn get(&self, id: &Key) -> Ref<Key, Value> {
-        let entry = self.resources.get(id);
-        match entry {
-            Some(entry) => Ref::new(entry.clone()),
-            None => Ref::none()
-        }
-    }
-
-    fn request_unchecked(&self, id: &Key) -> Ref<Key, Value> {
-        let mut shared = self.shared.borrow_mut();
-        // find resource in the requests
-        for request in shared.requests.iter() {
-            let found = match *request.borrow() {
-                Entry::Pending(ref rid, ..) => { *rid == *id }
-                _ => { panic!("Non-panding requests") }
-            };
-
-            if found {
-                return Ref::new(request.clone());
-            }
-        }
-
-        let request = shared.factory.request(id);
-        let request = Entry::Pending(id.clone(), request);
-        let request = Rc::new(RefCell::new(request));
-        shared.requests.push(request.clone());
-        Ref::new(request)
-    }
-
-    /// Requests a resource to be loaded. The resource is not loaded immediatelly
-    pub fn request(&self, id: &Key) {
-        if !self.resources.contains_key(id) {
-            self.request_unchecked(id);
-        }
-    }
-
-    /// Gets resource by the given id. If resource is not loaded an empty reference is returned
-    /// and a request is made to the loader.
-    pub fn get_or_request(&self, id: &Key) -> Ref<Key, Value> {
-        let resource = self.get(id);
-        if resource.is_some() {
-            resource
-        } else {
-            self.request_unchecked(id)
-        }
-    }
-
-    /// Creates the requested resources and stores them in the map.
-    pub fn update(&mut self) {
-        let mut shared = self.shared.borrow_mut();
-        let resources = &mut self.resources;
-        let shared = &mut *shared;
-        {
-            let factory = &mut shared.factory;
-            shared.requests.retain(|request| {
-                let store = {
-                    if let Entry::Pending(ref id, _) = *request.borrow_mut() {
-                        factory.create(id).map(|v| (id.clone(), v))
-                    } else {
-                        panic!("Non-pending requests")
-                    }
-                };
-
-                if let Some((id, value)) = store {
-                    *request.borrow_mut() = Entry::Ready(value);
-                    resources.insert(id, request.clone());
-                    false
-                } else {
-                    true
-                }
-            });
-        }
-    }
-
-    /// Removes unreferenced resources.
-    pub fn drain_unused(&mut self) {
-        let mut shared = self.shared.borrow_mut();
-        shared.requests.clear();
-        self.resources.retain(|_, v| {
-            Rc::strong_count(v) > 1
-        });
+impl<Key: Id, Value: Data> Drop for Ref<Key, Value> {
+    fn drop(&mut self) {
+        self.release();
     }
 }
--- engine/tests/store.rs
+++ engine/tests/store.rs
@@ -1,5 +1,8 @@
 extern crate dragorust_engine;
 
+use std::thread;
+use std::sync::Arc;
+
 use dragorust_engine::*;
 use container::store;
 
@@ -30,41 +33,106 @@ impl store::Factory<TestDataId, TestData> for TestFactory {
 
 /// Test datat store
 type TestStore = store::Store<TestDataId, TestData>;
+//type TestRef = store::Ref<TestDataId, TestData>;
 
 
 #[test]
 fn store_simple() {
-    let mut store = TestStore::new(TestFactory);
-    assert!(store.get(&TestDataId(0)).is_none());
-    assert!(!store.has_request());
-    let r0 = store.get_or_request(&TestDataId(0));
-    assert!(r0.is_pending());
-    store.request(&TestDataId(1));
-    assert!(store.len() == 0);
-    assert!(store.request_len() == 2);
+    let store = TestStore::new(TestFactory);
+    let mut r0;// = TestRef::none();
+    let mut r1;// = TestRef::none();
+
+    {
+        let store_use = store.read();
+        assert!(store_use.get(&TestDataId(0)).is_none());
+        //assert!(!store_use.has_request());
+        r0 = store_use.get_or_request(&TestDataId(0));
+        assert!(r0.is_pending());
+        store_use.request(&TestDataId(1));
+        //assert!(store_use.len() == 0);
+        //assert!(store_use.request_len() == 2);
+    }
 
     store.update();
-    assert!(store.request_len() == 0);
-    assert!(store.len() == 2);
-    assert!(!store.has_request());
-    assert!(store.get(&TestDataId(0)).is_ready());
-    assert!(store.get(&TestDataId(1)).is_ready());
-    assert!(r0.is_ready());
-    assert!(store.get(&TestDataId(0)) == r0);
+
     {
-        let r1 = store.get(&TestDataId(1));
+        let store_use = store.read();
+
+        //assert!(store.request_len() == 0);
+        //assert!(store.len() == 2);
+        //assert!(!store.has_request());
+        assert!(store_use.get(&TestDataId(0)).is_ready());
+        assert!(store_use.get(&TestDataId(1)).is_ready());
+        assert!(r0.is_ready());
+        //assert!(store_use.get(&TestDataId(0)) == r0);
+        r1 = store_use.get(&TestDataId(1));
         assert!(r1.is_some());
-        assert!(*r1.borrow() == store::Entry::Ready(TestData(format!("id: {}", 1))));
-        store.drain_unused();
-        assert!(store.get(&TestDataId(1)) == r1);
+        //assert!(*r1.borrow() == TestData(format!("id: {}", 1)));
+    }
+
+    store.drain_unused();
+
+    {
+        //let store_use = store.read();
+        //assert!(store_use.get(&TestDataId(1)) == r1);
         assert!(r0.is_some());
         assert!(r1.is_some());
+        r1.release();
     }
+
     store.drain_unused();
+
     assert!(r0.is_some());
-    assert!(store.get(&TestDataId(1)).is_none());
-    assert!(store.request_len() == 0);
-    assert!(store.len() == 1);
+    {
+        let store_use = store.read();
+        assert!(r0.is_some());
+        assert!(store_use.get(&TestDataId(1)).is_none());
+        //assert!(store.request_len() == 0);
+        //assert!(store.len() == 1);
+    }
+
+    r0.release();
+    store.drain_unused();
+    //assert!(store.is_empty());
 }
 
+#[test]
+fn store_multi_threaded() {
+    let store = Arc::new(TestStore::new(TestFactory));
 
+    {
+        let mut tp = vec!();
+        for i in 0..10 {
+            let s = store.clone();
+            tp.push(thread::spawn(move || {
+                let store_use = s.read();
+                assert!(store_use.get(&TestDataId(0)).is_none());
+                assert!(store_use.get_or_request(&TestDataId(1)).is_pending());
+                assert!(store_use.get_or_request(&TestDataId(100 + i)).is_pending());
+                println!("request {}", i);
+            }));
+        }
+        for t in tp.drain(..) {
+            t.join().unwrap();
+        }
+    }
+
+    store.update();
+
+    {
+        let mut tp = vec!();
+        for i in 0..10 {
+            let s = store.clone();
+            tp.push(thread::spawn(move || {
+                let store_use = s.read();
+                assert!(store_use.get(&TestDataId(0)).is_none());
+                assert!(store_use.get(&TestDataId(1)).is_ready());
+                assert!(store_use.get_or_request(&TestDataId(100 + i)).is_ready());
+                println!("request {}", i);
+            }));
+        }
+        for t in tp.drain(..) {
+            t.join().unwrap();
+        }
+    }
+}
