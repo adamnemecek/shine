6f59c3ac91257f0e2023f911ce69aafa2d75eb4a
gzp <z.p.gaal.devel@gmail.com>
2017-11-24 21:03:05 +0100

    image store

--- engine/src/world/imagestore.rs
+++ engine/src/world/imagestore.rs
@@ -1,5 +1,6 @@
 #![deny(missing_copy_implementations)]
 
+use std::sync::Arc;
 use std::path::Path;
 use container::store;
 use image;
@@ -16,49 +17,55 @@ impl Id {
 
 impl store::Id for Id {}
 
-
-/// The Image resource.
-pub type Image = image::DynamicImage;
+/// The Image resources.
+/// Error and pending images are shared. As they can be requested from multiple threads at the same time,
+/// AtomicReferenceCounter (Arc) is used. The loaded resources are unique and no reference counteng is required.
+pub enum Image {
+    Pending(Arc<image::DynamicImage>),
+    Missing(Arc<image::DynamicImage>),
+    Ready(image::DynamicImage),
+}
 
 impl store::Data for Image {}
 
 
 /// Factory to load images by the resource names
-struct ImageLoader;
+struct ImageLoader {
+    pending_image: Arc<image::DynamicImage>,
+    missing_image: Arc<image::DynamicImage>,
+}
 
 impl ImageLoader {
-    fn create_error_image(&self) -> Image {
-        image::DynamicImage::new_rgba8(4, 4)
-    }
-
-    fn create_pending_image(&self) -> Image {
-        image::DynamicImage::new_rgba8(4, 4)
+    fn new() -> ImageLoader {
+        ImageLoader {
+            pending_image: Arc::new(image::DynamicImage::new_rgba8(4, 4)),
+            missing_image: Arc::new(image::DynamicImage::new_rgba8(4, 4)),
+        }
     }
 }
 
 impl store::Factory<Id, Image> for ImageLoader {
     fn request(&mut self, _id: &Id) -> Image {
-        self.create_pending_image()
+        Image::Pending(self.pending_image.clone())
     }
 
     fn create(&mut self, id: &Id) -> Option<Image> {
         let path = Path::new(&id.0);
         let im = image::open(&path);
         if im.is_ok() {
-            Some(im.unwrap())
+            Some(Image::Ready(im.unwrap()))
         } else {
-            Some(self.create_error_image())
+            Some(Image::Pending(self.missing_image.clone()))
         }
     }
 }
 
 
-/// Creates the store the manage images.
+/// Creates the store to manage images.
 pub fn create() -> ImageStore {
-    ImageStore::new(ImageLoader)
+    ImageStore::new(ImageLoader::new())
 }
 
 pub type ImageStore = store::Store<Id, Image>;
 pub type ImageRef = store::Index<Id, Image>;
 pub type ImageId = Id;
-
