d0d5e3935f316c7ec42825422b41da6354bc4368
gzp <z.p.gaal.devel@gmail.com>
2018-06-11 10:28:56 +0200

    csmat stress test

--- graph/tests/csmat.rs
+++ graph/tests/csmat.rs
@@ -4,7 +4,7 @@ extern crate log;
 extern crate env_logger;
 extern crate rand;
 
-//use rand::Rng;
+use rand::Rng;
 
 use shine_graph::csmat::*;
 
@@ -52,27 +52,57 @@ fn csr_simple_<M: CSMat<Item = (usize, usize)>>(mut matrix: M) {
     }
 }
 
-fn csr_stress_<M: CSMat<Item = (usize, usize)>>(mut matrix: M) {
-    let mx = vec![vec![0; 128]];
-    let cnt = 70;
+fn csr_stress_<M: CSMat<Item = (usize, usize)>>(mut matrix: M, test_size: usize) {
+    let w = test_size;
+    // fill the 2/3-rd of the array
+    let cnt = w * w * 2 / 3;
 
-    let rng = rand::task_rng();
-    for i in 0..cnt {
-        let r = rng.gen_integer_range(0usize, 128);
-        let c = rng.gen_integer_range(0usize, 128);
-        if mx[r][c] == 1 {
-            assert_eq!(matrix.get(r, c), None);
-            matrix.add(r, c, (r, c));
-        } else {
+    let mut mx = vec![0; w * w];
+
+    let mut rng = rand::thread_rng();
+    for _ in 0..cnt {
+        let r = rng.gen_range(0usize, 128);
+        let c = rng.gen_range(0usize, 128);
+        let v = &mut mx[r * w + c];
+        if *v == 1 {
             assert_eq!(matrix.get(r, c), Some(&(r, c)));
+        } else {
+            assert_eq!(matrix.get(r, c), None);
         }
+        *v = 1;
+        matrix.add(r, c, (r, c));
     }
 
     for r in 0..128 {
         for c in 0..128 {
-            if mx[r][c] == 0 {
+            if mx[r * w + c] == 0 {
                 assert_eq!(matrix.get(r, c), None);
             } else {
+                assert_eq!(matrix.get(r, c), Some(&(r, c)));
+            }
+        }
+    }
+}
+
+fn csr_fill_<M: CSMat<Item = (usize, usize)>>(mut matrix: M) {
+    let w = 17;
+    let mut idx = vec![(0, 0); w * w];
+
+    for r in 0..w {
+        for c in 0..w {
+            idx[r * w + c] = (r, c);
+        }
+    }
+
+    for _ in 0..100 {
+        rand::thread_rng().shuffle(&mut idx);
+
+        for i in idx.iter() {
+            matrix.add(i.0, i.1, *i);
+        }
+
+        for r in 0..w {
+            for c in 0..w {
                 assert_eq!(matrix.get(r, c), Some(&(r, c)));
             }
         }
@@ -83,19 +113,25 @@ fn csr_stress_<M: CSMat<Item = (usize, usize)>>(mut matrix: M) {
 fn csr_simple() {
     let _ = env_logger::try_init();
 
-    trace!("CSVecMat - row");
-    csr_simple_(CSVecMat::<(usize, usize)>::new_row());
-    csr_stress_(CSVecMat::<(usize, usize)>::new_row());
+    for _ in 0..10 {
+        trace!("CSVecMat - row");
+        csr_simple_(CSVecMat::<(usize, usize)>::new_row());
+        csr_stress_(CSVecMat::<(usize, usize)>::new_row(), 2048);
+        csr_fill_(CSVecMat::<(usize, usize)>::new_row());
 
-    trace!("CSVecMat - col");
-    csr_simple_(CSVecMat::<(usize, usize)>::new_column());
-    csr_stress_(CSVecMat::<(usize, usize)>::new_column());
+        trace!("CSVecMat - col");
+        csr_simple_(CSVecMat::<(usize, usize)>::new_column());
+        csr_stress_(CSVecMat::<(usize, usize)>::new_column(), 2048);
+        csr_fill_(CSVecMat::<(usize, usize)>::new_column());
 
-    trace!("CSArenaMat - row");
-    csr_simple_(CSArenaMat::<(usize, usize)>::new_row());
-    csr_stress_(CSArenaMat::<(usize, usize)>::new_row());
+        trace!("CSArenaMat - row");
+        csr_simple_(CSArenaMat::<(usize, usize)>::new_row());
+        csr_stress_(CSArenaMat::<(usize, usize)>::new_row(), 2048);
+        csr_fill_(CSArenaMat::<(usize, usize)>::new_row());
 
-    trace!("CSArenaMat - col");
-    csr_simple_(CSArenaMat::<(usize, usize)>::new_column());
-    csr_stress_(CSArenaMat::<(usize, usize)>::new_column());
+        trace!("CSArenaMat - col");
+        csr_simple_(CSArenaMat::<(usize, usize)>::new_column());
+        csr_stress_(CSArenaMat::<(usize, usize)>::new_column(), 128);
+        csr_fill_(CSArenaMat::<(usize, usize)>::new_column());
+    }
 }
