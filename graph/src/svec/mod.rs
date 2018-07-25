7db6983c80573d358840d746008b0620d93a7daa
gzp <z.p.gaal.devel@gmail.com>
2018-07-25 15:12:16 +0200

    iterator and join test for vector

--- graph/src/svec/mod.rs
+++ graph/src/svec/mod.rs
@@ -1,13 +1,13 @@
 mod densestore;
 mod hashstore;
+mod join;
 mod sparsevector;
 mod store;
 mod unitstore;
 
-pub mod join;
-
 pub use self::densestore::*;
 pub use self::hashstore::*;
+pub use self::join::*;
 pub use self::sparsevector::*;
 pub use self::store::*;
 pub use self::unitstore::*;
