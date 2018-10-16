59ce73fab187e4d52f56ba3ae3389e6fb7dd1e7c
gzp <z.p.gaal@gmail.com>
2018-10-17 00:43:48 +0200

    failing triangulation, to be checked

--- tri/tests/regression.rs
+++ tri/tests/regression.rs
@@ -43,3 +43,31 @@ fn issue39_2() {
     assert_eq!(tri.dimension(), 2);
     assert_eq!(Checker::new(&tri).check(None), Ok(()), "{:?}", tri);
 }
+
+#[test]
+#[ignore]
+fn issue39_3() {
+    // todo: test if it failes for numeric issues (inexact)
+    init_test_logger(module_path!());
+
+    let mut tri = SimpleTri::new();
+
+    let pnts = vec![
+        (0.0, 0.0),
+        (0.0, -1.0),
+        (16.44394, 98.83319),
+        (57.17581, 47.452515),
+        (0.0, 1.0),
+        (58.0, 47.0),
+        (17.068817, 98.04495),
+    ];
+
+    {
+        let mut builder = Builder::new(&mut tri);
+        for &(x, y) in pnts.iter() {
+            builder.add_vertex(TriPos(x, y), None);
+        }
+    }
+    assert_eq!(tri.dimension(), 2);
+    assert_eq!(Checker::new(&tri).check(None), Ok(()), "{:?}", tri);
+}
