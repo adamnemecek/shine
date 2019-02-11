05dd42253a4cb84bc1c740b29dd91369806f2ffe
gzp <z.p.gaal.devel@gmail.com>
2019-02-11 16:18:29 +0100

    rendy hacks [ci skip]

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,66 @@
-pub fn main() {}
+use rendy::factory::{Config as RendyConfig, Factory};
+use shine_ecs::World;
+use std::sync::Arc;
+use winit::{Event, EventsLoop, VirtualKeyCode, WindowBuilder, WindowEvent};
+
+mod render;
+
+fn main() {
+    ::std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Warn)
+        .filter_module("shine", log::LevelFilter::Trace)
+        .init();
+
+    let mut world = World::new();
+
+    let config: RendyConfig = Default::default();
+    let mut factory: Factory<render::Backend> = Factory::new(config).unwrap();
+    let mut event_loop = EventsLoop::new();
+
+    let window = Arc::new(WindowBuilder::new().with_title("Shine").build(&event_loop).unwrap());
+    let mut graph: Option<render::Graph> = None;
+
+    let mut is_closing = false;
+    loop {
+        let mut release_graph = false;
+        factory.cleanup();
+        event_loop.poll_events(|e| match e {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => is_closing = true,
+                /*WindowEvent::Resized { .. } => {
+                    release_graph = true;
+                }*/
+                WindowEvent::KeyboardInput { input, .. } => {
+                    if input.virtual_keycode == Some(VirtualKeyCode::Escape) {
+                        is_closing = true;
+                    }
+                    if input.virtual_keycode == Some(VirtualKeyCode::F11) {
+                        release_graph = true;
+                    }
+                }
+                _ => {}
+            },
+            _ => {}
+        });
+
+        if release_graph || is_closing {
+            if let Some(graph) = graph.take() {
+                graph.dispose(&mut factory, &mut world);
+            }
+        }
+
+        if is_closing {
+            break;
+        }
+
+        if graph.is_none() {
+            let surface = factory.create_surface(window.clone());
+            graph = Some(render::init(&mut factory, surface, &mut world));
+        }
+
+        if let Some(ref mut graph) = graph {
+            graph.run(&mut factory, &mut world);
+        }
+    }
+}
