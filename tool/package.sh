b06d2d2cfd60d8ff3986589267066b6be1bdf598
gzp <z.p.gaal.devel@gmail.com>
2019-05-03 15:02:13 +0200

    fix workspace, publish helper

---
+++ tool/package.sh
@@ -0,0 +1,13 @@
+cargo publish --manifest-path crates\shine-gltf-macro\Cargo.toml
+cargo publish --manifest-path crates\shine-gltf\Cargo.toml
+cargo publish --manifest-path crates\shine-testutils\Cargo.toml
+cargo publish --manifest-path crates\shine-stdext\Cargo.toml
+cargo publish --manifest-path crates\shine-math\Cargo.toml
+cargo publish --manifest-path crates\shine-graph-macro\Cargo.toml
+cargo publish --manifest-path crates\shine-graph\Cargo.toml
+cargo publish --manifest-path crates\shine-ecs-macro\Cargo.toml
+cargo publish --manifest-path crates\shine-ecs\Cargo.toml
+cargo publish --manifest-path crates\shine-input\Cargo.toml
+cargo publish --manifest-path crates\shine-shard\Cargo.toml
+cargo publish --manifest-path crates\shine\Cargo.toml
+
